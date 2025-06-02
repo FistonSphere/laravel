563c53911650fb849f6e40e372338d90ae2f921c
fistonsano04 <sanofiston04@gmail.com>
2025-06-02 12:25:12 +0200

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route('/login', function(){
+Route::get('/login', function () {
     return view('auth.login');
-});
+})->name('login');
